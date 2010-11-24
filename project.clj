d9cf70aab8d34eb61a26edba20d97d831d32013a
badjer <brian.dejong@gmail.com>
2010-11-23 21:31:12 -0800

    Basics done - connect to db, connect to yahoo

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject stoop "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                [org.clojure/clojure-contrib "1.2.0"]
+				[postgresql "9.0-801.jdbc4"]
+				[org.clojars.badjer/clojure-yahoo-finance "1.0.0-SNAPSHOT"]])
--- src/stoop/core.clj
+++ src/stoop/core.clj
@@ -1,1 +1,15 @@
-(ns stoop.core)
+(ns stoop.core
+	(:use [clojure.contrib.sql])
+	(:require [clojure-yahoo-finance.core :as finance]))
+
+(def db {:classname "org.postgresql.Driver"
+		:subprotocol "postgresql"
+		:subname "//localhost:5432/stoop"
+		:user "postgres"
+		:password "abc123"})
+
+(defn read-db []
+	"Reads everything in the db"
+	(with-connection db 
+		(with-query-results rs ["select * from dpoint"]
+			(dorun (map #(println %) rs)))))
