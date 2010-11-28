4f3083413b0b2fae5194e20e55d9984a765bca9c
badjer <brian.dejong@gmail.com>
2010-11-27 20:54:03 -0800

    Now parses properly, and writes to the db

--- src/stoop/core.clj
+++ src/stoop/core.clj
@@ -1,6 +1,9 @@
 (ns stoop.core
 	(:use [clojure.contrib.sql])
-	(:require [clojure-yahoo-finance.core :as finance]))
+	(:import [java.sql.Date]
+			[org.joda.time LocalDate])
+	(:require [clojure-yahoo-finance.core :as finance]
+				[clojure.contrib.string :as string]))
 
 (def db {:classname "org.postgresql.Driver"
 		:subprotocol "postgresql"
@@ -13,3 +16,39 @@
 	(with-connection db 
 		(with-query-results rs ["select * from dpoint"]
 			(dorun (map #(println %) rs)))))
+
+(defn- sql_date [s]
+	"Converts a JODA date to a sql date for use by clojure.contrib.sql
+	Taken from www.paullegato.com/blog/clojure-joda-sql-date-time"
+	(java.sql.Date. (.. (LocalDate. s) toDateMidnight toInstant getMillis)))
+
+(defn parse-quote [sym quotestring]
+	"Converts a quotestring into a map of data that can be used with the db"
+	(let [strs (rest (string/split-lines quotestring))
+			parts (fn [line] (string/split #"," line))
+			dict (fn [ps] (hash-map 
+				:symbol sym
+				:date (sql_date (first ps))
+				:openp (BigDecimal. (second ps))
+				:highp (BigDecimal. (nth ps 2))
+				:lowp (BigDecimal. (nth ps 3))
+				:closep (BigDecimal. (nth ps 4))
+				:volume (BigDecimal. (nth ps 5))
+				:adjp (BigDecimal. (nth ps 6))))]
+		(map #(dict (parts %)) strs)))
+
+(defn insert-db [dpoints]
+	"Inserts a collection of datapoints (dpoint) into the database"
+	(with-connection db (apply insert-records "dpoint" dpoints)))
+
+(defn load-db [start end sym]
+	"Loads historical stock data for sym from start to end to the database"
+	(let [raw (finance/blocking-query start end [sym])
+		  quotestring (raw sym)
+		  data (parse-quote sym quotestring)]
+		(insert-db data)))
+
+
+; To load namespace at repl:
+; (require '(clojure.contrib [string :as string]))
+
